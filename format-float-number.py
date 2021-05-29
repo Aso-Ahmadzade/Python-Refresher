529d1b0bb7ad94ddc21bb76cd8bcfe1627aa5d32
Hosein-Ahmadzade <64315919+Hosein-Ahmadzade@users.noreply.github.com>
2021-05-29 15:58:58 +0430

    format-float-number added

---
+++ format-float-number.py
@@ -0,0 +1,11 @@
+my_input = int(input("Enter a number: "))
+
+my_operation = my_input / 10.8
+
+print(f'The result is: {my_operation}')
+
+# Use ~ :.<numbers that we want after the point>f ~ for formatting float numbers.
+print(f'The result is: {my_operation:.4f}')
+print(f'The result is: {my_operation:.2f}')
+print(f'The result is: {my_operation:.1f}')
+print(f'The result is: {my_operation:.0f}')
